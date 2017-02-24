02aaed9b1ca1bc42e233e57b827daf44c24293fc
Mark Staebler <markstaebler@gmail.com>
2017-02-24 13:10:15 -0800

    validate function

--- src/validate.js
+++ src/validate.js
@@ -1,9 +1,24 @@
 function validate(timestamp) {
     var result = {
-                    unix: 'unix',
-                    natural: 'natural'
+                    unix: false,
+                    natural: false
                   };
     //validate timestamp here
+    var valid = new Date(timestamp).getTime() > 0;
+    if(isNaN(timestamp)){
+        if(valid){
+            result.natural = timestamp;
+            result.unix = Date.parse(timestamp)/1000;
+        }
+    }else{
+        if(valid)
+        result.unix = timestamp;
+        var date = new Date(timestamp*1000),
+            hours = date.getHours(),
+            minutes = "0" + date.getMinutes(),
+            seconds = "0" + date.getSeconds();
+        result.natural = hours + ':' + minutes.substr(-2) + '+' + seconds.substr(-2);
+    }
     return result
 }
 
