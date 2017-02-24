5b4ef8118b9088a211b2b51719dbf4fecf3de10e
Mark Staebler <markstaebler@gmail.com>
2017-02-24 12:56:41 -0800

    express setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,27 @@
 const express = require('express'),
       app = express(),
       path = require('path'),
-      morgan = require('morgan');
+      morgan = require('morgan'),
+      validate = require('./validate');
+
+app.listen(process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/:timestamp', (req, res, next) => {
+    return res.json(validate(req.params.timestamp));
+});
+
+app.use(function(err, req, res, next) {
+      res.error = err;
+      res.status(err.status || 500);
+      res.json({
+          message: err.message
+      });
+      next(err);
+  });
---
+++ src/validate.js
@@ -0,0 +1,10 @@
+function validate(timestamp) {
+    var result = {
+                    unix: 'unix',
+                    natural: 'natural'
+                  };
+    //validate timestamp here
+    return result
+}
+
+module.exports = validate;
